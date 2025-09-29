01ad99c0de4473c69f494d4d847b420f73bcb635
Yassine Ghediri <yassinegh3diri@gmail.com>
2025-09-29 20:07:46 +0100

    Add welcome message with print_slow on startup
    Introduced a welcome message using the print_slow function when the application starts to inform users how to interact with Jarvis.

--- src/main.py
+++ src/main.py
@@ -3,11 +3,13 @@ from src.utils.executor import handle
 from utils.speech import listen_from_mic, speak_and_print
 from utils.executor import handle
 from utils.jarvis import initialize
+from utils.text import print_slow
 from memory import context
 
 def main():
     global context
     initialize()
+    print_slow("Welcome. Say 'Jarvis', 'Hey Jarvis' or any phrase containing the name to speak.'")
     while True:
         prompt = listen_from_mic()
         if len(context) > 25:
